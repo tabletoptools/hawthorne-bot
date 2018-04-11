a1f6a880d910e3a02f6fbed15549a3c57141150e
Carlo Field <cfi@bluesky-it.ch>
2018-04-11 13:32:24 +0200

    Update docker_push.sh
    Signed-off-by: Carlo Field <cfi@bluesky-it.ch>

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin bash
 gcloud docker -- push us.gcr.io/hawthorne-197812/hawthorne-bot:${TRAVIS_TAG};
-gcloud beta compute instances update-container hawthorne-001 --zone=us-east1-b --container-image=us.gcr.io/hawthorne-197812/hawthorne-bot:${TRAVIS_TAG};
+# gcloud beta compute instances update-container hawthorne-001 --zone=us-east1-b --container-image=us.gcr.io/hawthorne-197812/hawthorne-bot:${TRAVIS_TAG};
