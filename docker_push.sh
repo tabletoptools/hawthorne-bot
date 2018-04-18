dad8bc0d43956317c4e56289db7f61a781faadd2
Carlo Field <cfi@bluesky-it.ch>
2018-04-18 19:19:36 +0200

    Fix docker push
    Signed-off-by: Carlo Field <cfi@bluesky-it.ch>

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,4 @@
-#!/bin bash
+#!/bin/bash
+echo ${BUILD_VERSION};
 gcloud docker -- push us.gcr.io/hawthorne-197812/hawthorne-bot:${BUILD_VERSION};
 # gcloud beta compute instances update-container hawthorne-001 --zone=us-east1-b --container-image=us.gcr.io/hawthorne-197812/hawthorne-bot:${BUILD_VERSION};
